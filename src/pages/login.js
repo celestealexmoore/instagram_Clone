4bc8b1ae6778ac02f1f0e8af1eb59bb200a9dc25
cMoore <celestealexmoore@gmail.com>
2022-07-24 19:52:33 -0500

    Added images, coming to a stopping point for today.

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,8 @@
 import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
+import iPhone from "../images/iphone-with-profile.png";
+import logo from "../images/logo.png";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -19,8 +21,14 @@ export default function Login() {
   }, []);
 
   return (
-  <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-    <p>I have no idea!</p>
-  </div>
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      {/* <img src={logo} alt="Instagram Logo"></img> */}
+      <div className="flex w-3/5">
+        <img src={iPhone} alt="iPhone with Instagram Opened"></img>
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>I will be the form!</p>
+      </div>
+    </div>
   );
 }
