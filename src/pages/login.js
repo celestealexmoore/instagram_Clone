5234f94f77fb1665edeef8e7ae32d400cca62694
cMoore <celestealexmoore@gmail.com>
2022-07-25 01:15:18 -0500

    Login page email and password validation logic

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,9 @@
 import { useState, useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import iPhone from "../images/iphone-with-profile.png";
 import logo from "../images/logo.png";
+import * as ROUTES from "../constants/routes";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -14,7 +15,19 @@ export default function Login() {
   const [error, setError] = useState("");
   const isInvalid = password === "" || emailAddress === "";
 
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      navigate(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress("");
+      setPassword("");
+      setError(error.message);
+//password is test123
+    }
+  };
 
   useEffect(() => {
     document.title = "Login - Instagram";
@@ -22,12 +35,57 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      {/* <img src={logo} alt="Instagram Logo"></img> */}
       <div className="flex w-3/5">
         <img src={iPhone} alt="iPhone with Instagram Opened"></img>
       </div>
+
       <div className="flex flex-col w-2/5">
-        <p>I will be the form!</p>
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img
+              src={logo}
+              alt="iPhone with Instagram Opened"
+              className="mt-2 w-6/12 mb-4"
+            ></img>
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && "opacity-50"
+              }`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+          <p className="text-sm">
+            Don't have an account?{` `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
