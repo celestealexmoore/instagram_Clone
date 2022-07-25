e06abade59ead0f71f9bd4956533f6d75a6d498c
cMoore <celestealexmoore@gmail.com>
2022-07-24 19:05:51 -0500

    installed a few more dependencies + tailwind

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
+import './styles/app.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <FirebaseContext.Provider value={{firebase, FieldValue}}>
     <App />
     </FirebaseContext.Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
